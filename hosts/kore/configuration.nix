eb1968602a653dfdb94814217e0d9ec261fe7e43
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-12-30 13:33:34 -0800

    hosts/kore: use the last known working mongodb

--- hosts/kore/configuration.nix
+++ hosts/kore/configuration.nix
@@ -20,6 +20,21 @@
   services.unifi = {
     enable = true;
     jrePackage = pkgs.jre8_headless;
+
+    # Use the last known working mongodb package.
+    # https://github.com/NixOS/nixpkgs/issues/75133
+    mongodbPackage = let
+      channelRelease = "nixos-19.09pre190687.3f4144c30a6";  # last known working mongo
+      channelName = "unstable";
+      url = "https://releases.nixos.org/nixos/${channelName}/${channelRelease}/nixexprs.tar.xz";
+      sha256 = "040f16afph387s0a4cc476q3j0z8ik2p5bjyg9w2kkahss1d0pzm";
+
+      pinnedNixpkgsFile = builtins.fetchTarball {
+        inherit url sha256;
+      };
+
+      pinnedNixpkgs = import pinnedNixpkgsFile {};
+    in pinnedNixpkgs.mongodb;
   };
 
   nixpkgs.config.allowUnfree = true;
