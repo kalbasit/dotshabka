8b5449762907258525f188527a7fdba2e00fe291
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-12-31 08:48:53 -0800

    hosts/zeus: put the hashedPassword and the SSH auth keys

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -4,6 +4,8 @@ with lib;
 
 let
   shabka = import <shabka> { };
+  dotshabka = import ../.. { };
+  hashedPassword = "$6$0bx5eAEsHJRxkD8.$gJ7sdkOOJRf4QCHWLGDUtAmjHV/gJxPQpyCEtHubWocHh9O7pWy10Frkm1Ch8P0/m8UTUg.Oxp.MB3YSQxFXu1";
 
   nasIP = "192.168.50.2";
 
@@ -86,9 +88,11 @@ in {
     enable = true;
 
     users = {
-      yl = { uid = 2000; isAdmin = true;  home = "/yl"; };
+      yl = { inherit hashedPassword; sshKeys = singleton dotshabka.external.kalbasit.keys; uid = 2000; isAdmin = true;  home = "/yl"; };
     };
   };
+
+  users.users.root.openssh.authorizedKeys.keys = singleton dotshabka.external.kalbasit.keys;
 
   shabka.hardware.machine = "zeus";
 
