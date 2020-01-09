723d6e3ad1d7e4800409c9da197b4352b24ff610
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-01-08 23:46:33 -0800

    hosts/xenia: create a new server

---
+++ hosts/xenia/configuration.nix
@@ -0,0 +1,120 @@
+{ pkgs, lib, ... }:
+
+with lib;
+
+let
+  shabka = import <shabka> { };
+  dotshabka = import ../.. { };
+  hashedPassword = "$6$0bx5eAEsHJRxkD8.$gJ7sdkOOJRf4QCHWLGDUtAmjHV/gJxPQpyCEtHubWocHh9O7pWy10Frkm1Ch8P0/m8UTUg.Oxp.MB3YSQxFXu1";
+
+in {
+  imports = [
+    ./hardware-configuration.nix
+
+    "${shabka.external.nixos-hardware.path}/common/cpu/intel"
+
+    <shabka/modules/nixos>
+
+    ./home.nix
+  ]
+  ++ (optionals (builtins.pathExists ./../../secrets/nixos) (singleton ./../../secrets/nixos));
+
+  # set the default locale and the timeZone
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "America/Los_Angeles";
+
+  networking.hostName = "xenia";
+
+  shabka.keyboard.layouts = [ "colemak" ];
+  shabka.neovim.enable = true;
+  shabka.virtualisation.docker.enable = true;
+
+  shabka.users = {
+    enable = true;
+
+    users = {
+      yl = { inherit hashedPassword; sshKeys = singleton dotshabka.external.kalbasit.keys; uid = 2000; isAdmin = true;  home = "/yl"; };
+    };
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = singleton dotshabka.external.kalbasit.keys;
+
+  #
+  # Network
+  #
+
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp4s0.useDHCP = true;
+
+  # networking.vlans = {
+  #   # The ADMIN interface
+  #   ifcadmin = {
+  #     id = 2;
+  #     interface = "enp0s31f6";
+  #   };
+  #
+  #   # SN0 interface
+  #   ifcns0 = {
+  #     id = 50;
+  #     interface = "enp2s0f0";
+  #   };
+  #
+  #   # SN1 interface
+  #   ifcns1 = {
+  #     id = 51;
+  #     interface = "enp2s0f1";
+  #   };
+  #
+  #   # SN2 interface
+  #   ifcns2 = {
+  #     id = 52;
+  #     interface = "enp4s0f0";
+  #   };
+  #
+  #   # SN3 interface
+  #   ifcns3 = {
+  #     id = 53;
+  #     interface = "enp4s0f1";
+  #   };
+  # };
+  #
+  # networking.interfaces = {
+  #   # turn off DHCP on all real interfaces, I use virtual networks.
+  #   enp2s0f0 = { useDHCP = false; };
+  #   enp2s0f1 = { useDHCP = false; };
+  #   enp4s0f0 = { useDHCP = false; };
+  #   enp4s0f1 = { useDHCP = false; };
+  #   enp0s31f6 = { useDHCP = false; };
+  #
+  #   # The ADMIN interface
+  #   ifcadmin = {
+  #     useDHCP = true;
+  #   };
+  #
+  #   # SN0 address
+  #   ifcns0 = {
+  #     useDHCP = true;
+  #   };
+  #
+  #   # SN1 address
+  #   ifcns1 = {
+  #     useDHCP = true;
+  #   };
+  #
+  #   # SN2 address
+  #   ifcns2 = {
+  #     useDHCP = true;
+  #   };
+  #
+  #   # SN3 address
+  #   ifcns3 = {
+  #     useDHCP = true;
+  #   };
+  # };
+
+  # This value detershabkas the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+}
