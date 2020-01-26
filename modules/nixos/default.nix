b42ae418533d39be4a61d8cbbb3fa718ac3547f0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-01-25 21:46:34 -0800

    nixos: force set nix.binaryCaches to remove cache.nixos.org over https

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -29,4 +29,11 @@ in
   };
 
   users.users.root.openssh.authorizedKeys.keys = singleton dotshabka.external.kalbasit.keys;
+
+  # override the binary caches to remove cache.nixos.org over https
+  nix.binaryCaches = mkForce [
+    "http://cache.nixos.org"
+    "http://yl.cachix.org"
+    "http://risson.cachix.org"
+  ];
 }
