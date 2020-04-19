7490c26249fa391022d44cae79c48cfb06a5b30d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-04-19 14:53:15 -0700

    move the nix.binaryCaches setting to my private repo

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -30,11 +30,4 @@ in
   };
 
   users.users.root.openssh.authorizedKeys.keys = singleton dotshabka.external.kalbasit.keys;
-
-  # override the binary caches to remove cache.nixos.org over https
-  nix.binaryCaches = mkForce [
-    "http://cache.nixos.org"
-    "http://yl.cachix.org"
-    "http://risson.cachix.org"
-  ];
 }
