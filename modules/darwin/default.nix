28984600b7c5461f4e3c7a741ca932d49a2e2b4a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-04-19 16:14:53 -0700

    darwin: do not force binary caches, let secrets do that

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -5,11 +5,4 @@ with lib;
 {
   shabka.keyboard.layouts = [ "colemak" ];
   shabka.fonts.enable = true;
-
-  # override the binary caches to remove cache.nixos.org over https
-  nix.binaryCaches = mkForce [
-    "http://cache.nixos.org"
-    "http://yl.cachix.org"
-    "http://risson.cachix.org"
-  ];
 }
