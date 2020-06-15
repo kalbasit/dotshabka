b947baf01ed9134be7af5d8e042c66e4e8569deb
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-06-15 09:38:01 -0700

    home/git: fix aliases broken by HM change
    Home manager is now escaping Git aliases properly, but that has broke my
    escaping hack to make it work.
    
    https://github.com/rycee/home-manager/commit/642d9ffe24eec1290e94d70e8129ba74922fddf2

--- modules/home/software/git.nix
+++ modules/home/software/git.nix
@@ -23,20 +23,20 @@ in {
     programs.git.aliases = {
       # list files which have changed since REVIEW_BASE
       # (REVIEW_BASE defaults to 'master' in my zshrc)
-      files     = "\"!git diff --name-only \$(git merge-base HEAD \\\"\${REVIEW_BASE:-master}\\\")\"";
+      files = ''!git diff --name-only ''$(git merge-base HEAD "''${REVIEW_BASE:-master}")'';
 
       # Same as above, but with a diff stat instead of just names
       # (better for interactive use)
-      stat      = "\"!git diff --stat \$(git merge-base HEAD \\\"\${REVIEW_BASE:-master}\\\")\"";
+      stat = ''!git diff --stat ''$(git merge-base HEAD ''${REVIEW_BASE:-master})'';
 
       # Open all files changed since REVIEW_BASE in Vim tabs
       # Then, run fugitive's :Gdiff in each tab, and finally
-      review    = "\"!nvim -p $(git files) +\\\"tabdo Gdiff \${REVIEW_BASE:-master}\\\"\"";
+      review = ''!nvim ''$(git files) +"tabdo Gdiff ''${REVIEW_BASE:-master}"'';
 
       # Same as the above, except specify names of files as arguments,
       # instead of opening all files:
       # git reviewone foo.js bar.js
-      reviewone = "\"!nvim -p +\\\"tabdo Gdiff \${REVIEW_BASE:-master}\\\"\"";
+      reviewone = ''!nvim -p +"tabdo Gdiff ''${REVIEW_BASE:-master}"'';
     };
 
     programs.git.extraConfig = {
